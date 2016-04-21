3f0549db56ab3ea6c4b6fa35fe7a5c0a8f20805d
rluko14 <rluko@bu.edu>
2016-04-21 14:44:11 -0400

    Update block_gamev3.py

--- block_gamev3.py
+++ block_gamev3.py
@@ -71,6 +71,53 @@ while 1:
         #Update screen
         pygame.display.flip() 
 
+import pygame
+
+windowWidth = 400
+windowHeight = 300
+lineThickness = 10
+
+black = (0,0,0)
+white = (255,255,255)
+
+ballDirX = -1
+ballDirY = -1
+
+gameDisplay = pygame.display.set_mode((windowWidth,windowHeight)) 
+pygame.display.set_caption('Pong')
+
+class Ballz:
+    
+    def __init__():
+        pygame.draw.rect(gameDisplay,white,ball)
+
+    def addBall(ball):
+        pygame.draw.rect(gameDisplay,white,ball)
+
+    def moveBall(ball_rect,ballDirX,ballDirY):
+        pygame.ball.x += ballDirX
+        ball.y += ballDirY
+        return ball
+
+    def windowCollision(ball, ballDirX, ballDirY):
+        if ball.top == (lineThickness) or ball.bottom == (windowHeight - lineThickness):
+            ballDirY = ballDirY * -1
+            if ball.left == (lineThickness) or ball.right == (windowWidth - lineThickness):
+                ballDirX = ballDirX * -1
+                return ballDirX, ballDirY
+   
+    def paddleCollision(ball, PADDLE1, PADDLE2, ballDirX):
+        if ballDirX == -1 and PADDLE1.right == ball.left and PADDLE1.top < ball.top and PADDLE1.bottom > ball.bottom:
+            return -1
+        elif ballDirX == 1 and PADDLE2.left == ball.right and PADDLE2.top < ball.top and PADDLE2.bottom > ball.bottom:
+            return -1
+        else: return 1
+        
+        
+ball_test = Ballz()       
+
+
+ball_test.moveBall()
 
 
 
